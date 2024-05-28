478e7247b10b3bbbb72c199358b8b75001f2aa62
Holm-Codes <eaa24mhj@students.eaaa.dk>
2024-05-28 13:43:04 +0200

    Stor update

--- js/js.js
+++ js/js.js
@@ -3,3 +3,12 @@
  * purpose: Behaviors
  **/
 console.log('Success: JavaScript from js/js.js running!')
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
